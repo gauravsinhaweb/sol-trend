1284d643ed15a55c969d0b2a9e607239ac44eab1
GAURAV SINHA <sinhagaurav.me@gmail.com>
2025-09-22 14:34:29 +0530

    style: nav

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -40,7 +40,7 @@ export const TopNav: React.FC<TopNavProps> = ({ className = '' }) => {
                             </div>
                         </div>
                     </div>
-                    <button className="bg-primary-100 hover:bg-primary-600 text-black px-4 py-2 rounded-xs transition-colors flex items-center space-x-2 font-medium">
+                    <button className="bg-primary-200 cursor-pointer text-black px-4 py-2 rounded-xs transition-colors flex items-center space-x-2 font-medium">
                         <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z" />
                         </svg>
